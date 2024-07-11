81279ad4e3e245961c995d3f15529a5f9db6a00c
Arthur Ortiz <Ortiz.Arthur.E@gmail.com>
2024-07-11 07:37:28 -0600

    Data Access
    Got the basics for a db connection to work. Utilized the options pattern for the connection string, and made a custom attribute.

---
+++ DataAccess/TaskTrackerDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskTracker.Models;
+
+namespace TaskTracker.DataAccess
+{
+  public class TaskTrackerDbContext : DbContext
+  {
+    public TaskTrackerDbContext(DbContextOptions<TaskTrackerDbContext> options) : base(options) { }
+
+    public DbSet<UserTask> UserTasks { get; set; }
+  }
+}
