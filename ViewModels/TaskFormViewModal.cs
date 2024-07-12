4b57f0654fa5a94de9536ccb9802b0ff86ee1dc2
Arthur Ortiz <Ortiz.Arthur.E@gmail.com>
2024-07-12 08:58:36 -0600

    Created dynamic Form
    Created the first iteration of the form used to create tasks, later I will test to see how well this edits the task.

---
+++ ViewModels/TaskFormViewModal.cs
@@ -0,0 +1,12 @@
+﻿using TaskTracker.Models;
+
+namespace TaskTracker.ViewModels
+{
+  public class TaskFormViewModal
+  {
+    public UserTask Task { get; set; } = new UserTask();
+    public string Action {  get; set; } = string.Empty;
+    public string ButtonText { get; set; } = string.Empty;
+    public string FormId { get; set; } = string.Empty;
+  }
+}
